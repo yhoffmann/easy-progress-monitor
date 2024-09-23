6f246fe75c12b8ed6abec91723d0ef80259f4101
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2024-09-23 17:21:07 +0200

    Introduced atomics, now works with multi-threaded applications.

--- include/ProgressMonitor.hpp
+++ include/ProgressMonitor.hpp
@@ -1,19 +1,28 @@
 #pragma once
 
 
+#include <stdlib.h>
+#include <atomic>
+#include <iostream>
+#include <fstream>
 #include <string>
 
 
-class ProgressMonitor
-{
-    unsigned int m_num_tasks;
-    unsigned int m_finished_tasks;
-    unsigned int m_last_percentage_logged;
+class ProgressMonitor {
+protected:
+    std::atomic_uint m_num_tasks;
+    std::atomic_uint m_finished_tasks;
+    std::atomic<float> m_last_percentage_logged;
     std::string m_message;
+
 public:
     void add_finished();
-    void print_progress_percentage();
+    void print_progress_percentage(std::ostream& stream = std::cout);
     ProgressMonitor() = delete;
-    ProgressMonitor(unsigned int num_tasks);
-    ProgressMonitor(unsigned int num_tasks, const std::string& message);
+    ProgressMonitor(uint num_tasks, const std::string& message = "Progress: ");
+    ProgressMonitor(const ProgressMonitor&) = default;
+    ProgressMonitor(ProgressMonitor&&) = default;
+    ProgressMonitor& operator=(const ProgressMonitor&) = default;
+    ProgressMonitor& operator=(ProgressMonitor&&) = default;
+    ~ProgressMonitor() = default;
 };
--- src/ProgressMonitor.cpp
+++ src/ProgressMonitor.cpp
@@ -3,31 +3,25 @@
 #include <math.h>
 
 
-void ProgressMonitor::add_finished ()
-{
+void ProgressMonitor::add_finished() {
     m_finished_tasks++;
 }
 
 
-void ProgressMonitor::print_progress_percentage()
-{
+void ProgressMonitor::print_progress_percentage (std::ostream& stream) {
     float percentage = std::floor(float(m_finished_tasks)/float(m_num_tasks)*100.0);
 
-    if (percentage != m_last_percentage_logged)
-    {
-        std::cout << m_message << percentage << "%\n";
+    if (percentage != m_last_percentage_logged) {
+        stream << m_message << percentage << "%\n";
         m_last_percentage_logged = percentage;
     }
 }
 
 
-ProgressMonitor::ProgressMonitor (unsigned int num_tasks)
-    : m_num_tasks(num_tasks), m_finished_tasks(0), m_last_percentage_logged(0), m_message("Progress: ")
-{
-}
-
-
 ProgressMonitor::ProgressMonitor (unsigned int num_tasks, const std::string& message)
-    : m_num_tasks(num_tasks), m_finished_tasks(0), m_last_percentage_logged(0), m_message(message)
+    : m_num_tasks(num_tasks)
+    , m_finished_tasks(0)
+    , m_last_percentage_logged(0)
+    , m_message(message)
 {
 }
