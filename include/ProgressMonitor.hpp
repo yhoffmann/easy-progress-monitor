9d412832ef43a15cbfdf9bc03508c81a692bbd9e
Yannik Hoffmann <117282045+yhoffmann@users.noreply.github.com>
2024-10-28 17:14:15 +0100

    Moved everything into .hpp file.

--- include/ProgressMonitor.hpp
+++ include/ProgressMonitor.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#ifndef PROGRESSMONITOR_HPP
+#define PROGRESSMONITOR_HPP
 
 
 #include <stdlib.h>
@@ -6,6 +8,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 
 class ProgressMonitor {
@@ -26,3 +29,30 @@ public:
     ProgressMonitor& operator=(ProgressMonitor&&) = default;
     ~ProgressMonitor() = default;
 };
+
+
+void ProgressMonitor::add_finished() {
+    m_finished_tasks++;
+}
+
+
+void ProgressMonitor::print_progress_percentage (std::ostream& stream) {
+    float percentage = std::floor(float(m_finished_tasks)/float(m_num_tasks)*100.0);
+
+    if (percentage != m_last_percentage_logged) {
+        stream << m_message << percentage << "%\n";
+        m_last_percentage_logged = percentage;
+    }
+}
+
+
+ProgressMonitor::ProgressMonitor (unsigned int num_tasks, const std::string& message)
+    : m_num_tasks(num_tasks)
+    , m_finished_tasks(0)
+    , m_last_percentage_logged(0)
+    , m_message(message)
+{
+}
+
+
+#endif // PROGRESSMONITOR_HPP
